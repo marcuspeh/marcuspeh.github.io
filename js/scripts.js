a309470e61786fe8d44bc9f9c0dcafc8a1047c9e
Marcus Peh <hkmpeh@gmail.com>
2022-02-16 21:59:44 +0800

    Update project and experience

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
-/*!
-    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
-    (function ($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
