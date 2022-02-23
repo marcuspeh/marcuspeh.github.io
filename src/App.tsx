c42f88b2c82ecc10d6d3b6ef77d01af376db1fe9
Marcus Peh <hkmpeh@gmail.com>
2022-02-23 10:05:37 +0800

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = (): JSX.Element => {
+  return (
+    <div className="App">
+      Hi
+    </div>
+  );
+}
+
+export default App;
