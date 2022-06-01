00d58b5d44bee052981a5617ed8178154e578284
marcuspeh <hkmpeh@gmail.com>
2022-06-02 00:47:50 +0800

    Add pictures for project

--- src/components/projectCard/ProjectCard.tsx
+++ src/components/projectCard/ProjectCard.tsx
@@ -6,30 +6,31 @@ interface projectInfo {
     techStacks: string[];
     description: string;
     projectLink: string;
+    image: string;
 }
 
 
 function generateTechStack(techStack: string) {
-    return <Button variant='outline-dark' size="sm" className='me-1 px-1 py-0' disabled>{techStack}</Button>
+    return <Button variant='outline-dark' size="sm" className='me-1 px-1 py-0 mb-1' disabled>{techStack}</Button>
 }
 
 
 const Project: React.FC<projectInfo> = (projectInfo): JSX.Element => {
     return (
         <div className="mb-4">
-            <Card className="mb-3" style={{ height: '100%' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ height: '100%' }}>
+                <Card.Img variant="top" src={projectInfo.image} />
                 <Card.Body>
                     <Card.Title>
                         {projectInfo.title}
                     </Card.Title>
-                    <Card.Subtitle className="mb-2">{projectInfo.techStacks.map(generateTechStack)}</Card.Subtitle>
+                    <Card.Subtitle className="mb-1">{projectInfo.techStacks.map(generateTechStack)}</Card.Subtitle>
                     <Card.Text>
                         {projectInfo.description}
                     </Card.Text>
                 </Card.Body>
-                <Card.Footer>
-                    <Button variant="secondary" href={projectInfo.projectLink} target="_blank"><i className="fab fa-github"></i> View repository</Button>
+                <Card.Footer className='pb-3'>
+                    <Button variant="secondary" href={projectInfo.projectLink} target="_blank"><i className="fab fa-github me-1"></i> View repository</Button>
                 </Card.Footer>
             </Card>
         </div>
--- src/pages/project/Project.tsx
+++ src/pages/project/Project.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 
+import profile from '../../assets/img/profile.jpeg';
+import _2048Demo from '../../assets/img/2048_demo.jpg';
+import aweDemo from '../../assets/img/awe_demo.png';
+import markeetDemo from '../../assets/img/markeet_demo.png';
+import mazeSolverDemo from '../../assets/img/mazesolver_demo.png';
+import splitlixDemo from '../../assets/img/splitlux_demo.png';
+
 import ProjectCard from '../../components/projectCard/ProjectCard';
 
 const Project: React.FC = (): JSX.Element => {
@@ -13,6 +20,7 @@ const Project: React.FC = (): JSX.Element => {
       <Row xs={1} md={2} lg={3} className="row justify-content-center">
           <ProjectCard 
             title="AROUND THE WORLD IN $80" 
+            image={aweDemo}
             techStacks={['Java', 'JavaFx']}
             description='Splitting bills during travel is often a hassle. Most travel groups often designate one person to pay. This method of settling payments poses a vexing task of splitting costs at the end of the day or on the spot. Our app effectively splits bills between different contacts to serve this purpose.' 
             projectLink='https://github.com/marcuspeh/Around_The_World_In_80' 
@@ -20,13 +28,15 @@ const Project: React.FC = (): JSX.Element => {
 
           <ProjectCard 
             title="POINT OF SALES SYSTEM" 
-            techStacks={['MongoDb', 'ExpressJS', 'ReactJs', 'NodeJs']}
+            image={markeetDemo}
+            techStacks={['MongoDb', 'ExpressJS', 'ReactJs', 'NodeJs (JavaScript)']}
             description='Designed with both the business and consumers interest at heart. It incorporates a online web system to help store manage their sales and inventory as well as a telegram bot to help users check for stock in stores.' 
             projectLink='https://github.com/marcuspeh/markeet' 
           />
           
           <ProjectCard 
             title="SPLITLUX" 
+            image={splitlixDemo}
             techStacks={['Flutter (dart)', 'Django (Python)']}
             description='Build using Flutter and Django, Splitlux targets travellers in sight of borders opening from the pandemic. It aims to expedites the process by splitting and monitoring bills in an efficient manner during travel.' 
             projectLink='https://github.com/marcuspeh/Splitlux' 
@@ -34,20 +44,23 @@ const Project: React.FC = (): JSX.Element => {
           
           <ProjectCard 
             title="2048 SOLVER" 
+            image={_2048Demo}
             techStacks={['Python']}
             description='The solver plays the 2048 game based on Monte-Carlo (MC) algorithm. Since each step in 2048 will generate a new tile in random position, MC is the best algorithm for it.' 
             projectLink='https://github.com/marcuspeh/2048solver' 
           />
           
           <ProjectCard 
-            title="AUTO TEXT WRITER" 
-            techStacks={['Java']}
-            description='The generator will take in sample text at first and form Markov Model based on the next letter/word that comes after it. Using the Markov Model, it will then generate the text of certain length as specified by the user.' 
-            projectLink='https://github.com/marcuspeh/Automated-Writing' 
+            title="IOT RGB LIGHTS" 
+            image={profile}
+            techStacks={['Nodejs (TypeScript)', "TypeOrm", "Koa", "Postgres", "Arduino (C++)"]}
+            description='This project consists of the server to control the RGB lights, the codes to control the arduino as well as the app to interact with the rgb led from phone.' 
+            projectLink='https://github.com/marcuspeh/IOT_RGB_Lights' 
           />
           
           <ProjectCard 
             title="MAZE SOLVER" 
+            image={mazeSolverDemo}
             techStacks={['Java']}
             description='The codes run based on breadth first search and stores the path. The number of rooms reachable with each step is stored in a hashmap while computing the shortest path to solve the maze. This will reduce the computation needed' 
             projectLink='https://github.com/marcuspeh/Maze-Solver' 
