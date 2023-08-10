608e3e5db3058bb70893282a957055c5158396cc
marcuspeh <hkmpeh@gmail.com>
2023-08-10 23:19:33 +0800

    Update styling

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,12 +15,12 @@ function NavBarDesktop() {
   return (
     <Grid xs={12} item className="flex-row items-end justify-start" sx={{display: {xs: 'none', md: 'flex'}}}>
       <Box className="mr-2">
-        <Button href="#landing" className="subtitle primary underline text-bold">Marcus</Button>
+        <Button href="#landing" className="subtitle primary underline font-black pl-0">Marcus</Button>
       </Box>
       {
         NavBarLinkData.map(data => (
           <Box className="mr-2" key={`${data.id}_desktop`}>
-            <Button href={data.link} className="text primary-text">{data.text}</Button>
+            <Button href={data.link} className="subtitle primary-text font-bold">{data.text}</Button>
           </Box>
         ))
       }
@@ -32,11 +32,11 @@ function NavBarMobile() {
   const [isOpen, setIsOpen] = React.useState(false)
 
   return (
-    <Grid xs={12} item className="flex-row items-center justify-start pl-3"  sx={{display: {xs: 'flex', md: 'none'}}}>
-      <Button aria-label="menu" variant="outlined" onClick={() => setIsOpen(true)} className="w-7">
+    <Grid xs={12} item className="flex-row items-center justify-start"  sx={{display: {xs: 'flex', md: 'none'}}}>
+      <Button aria-label="menu" variant="outlined" onClick={() => setIsOpen(true)} className="mr-2 primary border-color-primary">
         <MenuIcon />
       </Button>
-      <Button href="#landing" className="subtitle primary underline text-bold">Marcus</Button>
+      <Button href="#landing" className="subtitle primary font-black">Marcus</Button>
       <Drawer
         anchor={'top'}
         open={isOpen}
@@ -46,13 +46,14 @@ function NavBarMobile() {
           role="presentation"
           onClick={() => setIsOpen(false)}
           onKeyDown={() => setIsOpen(false)}
+          className="background"
         >
           <List>
             {
               NavBarLinkData.map(data => (
                 <ListItem disablePadding key={`${data.id}_mobile`}>
                   <ListItemButton href={data.link}>
-                    <ListItemText primary={data.text} />
+                    <ListItemText primary={data.text} className="text primary-dark" />
                   </ListItemButton>
                 </ListItem>            
               ))
@@ -67,10 +68,12 @@ function NavBarMobile() {
 function NavBar() {
     return (
       <Box className='flex flex-col items-center absolute top-4 w-screen'>
-        <Grid container className="container">
-          <NavBarDesktop />
-          <NavBarMobile />
-        </Grid>
+        <Box className="px-8 container">
+          <Grid container className="pb-2 border-b border-color-background">
+            <NavBarDesktop />
+            <NavBarMobile />
+          </Grid>          
+        </Box>
       </Box>
     )
 }
