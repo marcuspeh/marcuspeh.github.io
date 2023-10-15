0d4126288072aaedff08d4a6f3313ea4437b33a6
marcuspeh <hkmpeh@gmail.com>
2023-10-15 13:14:56 +0800

    Update layout file

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type {Metadata} from 'next';
+import {Inter} from 'next/font/google';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const inter = Inter({subsets: ['latin']});
+
+export const metadata: Metadata = {
+  title: 'Marcus Peh',
+  description: 'Portfolio for Marcus Peh',
+};
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
